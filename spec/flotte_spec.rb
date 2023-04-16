a4796d47d22570ef88e8393ac504524cbc975247
Julian Fahrer <julian@fahrer.net>
2023-04-15 18:18:56 -0700

    Test the version command

--- spec/flotte_spec.rb
+++ spec/flotte_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Flotte do
     expect(Flotte::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "prints the version" do
+    expect { Flotte::CLI::Main.new.invoke(:version) }.to output("#{Flotte::VERSION}\n").to_stdout
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "flotte"
+require "byebug"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
@@ -11,5 +12,21 @@ RSpec.configure do |config|
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
+  end
+
+  def capture_stdout
+    begin
+      original_stdout = $stdout
+      $stdout = StringIO.new
+      yield
+      byebug
+      result = $stdout.read
+    ensure
+      $stdout = STDOUT
+    end
+
+    byebug
+
+    result
   end
 end

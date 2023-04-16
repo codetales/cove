b9c03cb962702f2e3bb3ed0a3e6df2f9b8382d9a
Julian Fahrer <julian@fahrer.net>
2023-04-15 17:15:22 -0700

    Initial commit

---
+++ lib/flotte.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "flotte/version"
+
+module Flotte
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/flotte_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Flotte do
+  it "has a version number" do
+    expect(Flotte::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end

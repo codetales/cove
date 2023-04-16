5ed76edede85140b5ccda8da22be4c976af37066
Julian Fahrer <julian@fahrer.net>
2023-04-15 21:14:14 -0700

    mend

---
+++ spec/flotte/cli/main_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+RSpec.describe Flotte do
+
+  describe "#version" do
+    it "prints the version" do
+      expect { Flotte::CLI::Main.new.invoke(:version) }.to output("#{Flotte::VERSION}\n").to_stdout
+    end
+  end
+end
--- spec/flotte_spec.rb
+++ spec/flotte_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Flotte do
   it "has a version number" do
     expect(Flotte::VERSION).not_to be nil
   end
-
-  it "prints the version" do
-    expect { Flotte::CLI::Main.new.invoke(:version) }.to output("#{Flotte::VERSION}\n").to_stdout
-  end
 end

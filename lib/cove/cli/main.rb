34b097111eed3f44bfb55ce389070a38a001d0ae
Julian Fahrer <julian@fahrer.net>
2023-04-29 18:33:18 -0700

    Remove cruft code from testing

--- lib/cove/cli/main.rb
+++ lib/cove/cli/main.rb
@@ -12,34 +12,6 @@ module Cove
         Cove.output.puts Cove::VERSION
       end
 
-      desc "runit", "Run a command"
-      def runit
-        captured = {}
-        image = "alpine"
-        container_name = "test-#{SecureRandom.alphanumeric(5)}"
-        host = "root@test"
-        on(host) do |host|
-          output = capture(*Command::Docker::Container::Run.build(image: image, name: container_name, remove: false, interactive: true), {verbosity: Logger::INFO})
-
-          captured[host] = output
-        end
-
-        run_locally do
-          command = Cove::Command::SSH.wrap(host, [:docker, :container, :attach, container_name, ";", "echo", "'42424242'"])
-          info "Running #{command.join(" ")}"
-          Kernel.exec(*command)
-        end
-      end
-
-      desc "logs", "Tail logs"
-      def logs
-        host = "root@test"
-        on([host]) do |host|
-          SSHKit.config.output_verbosity = Logger::DEBUG
-          execute :docker, "logs", "-f", "goofy_meitner"
-        end
-      end
-
       desc "host", "Host specific commands"
       subcommand "host", Cove::CLI::Host
 

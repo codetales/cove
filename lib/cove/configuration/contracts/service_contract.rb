dcbe87ba472b2e70f56509c505af9d04b47843ab
Sarah Loui <ssarahloui@gmail.com>
2023-08-29 18:04:02 -1000

    Add tests that check for specific errors in ServiceContract and add validation for role name uniqueness

--- lib/cove/configuration/contracts/service_contract.rb
+++ lib/cove/configuration/contracts/service_contract.rb
@@ -22,17 +22,25 @@ module Cove
           end
         end
 
-        rule(:roles).each do
+        role_names = []
+
+        rule(:roles).each do |index:|
+          if index == 0
+            role_names.clear
+          end
+          key([:roles, index, :name]).failure("#{value[:name]} is not a valid name. The name for each role must be unique.") if role_names.include?(value[:name])
+          role_names << value[:name]
           if value[:ingress].present?
-            value[:ingress].each_with_index do |port, ingress_index|
-              if port[:type].eql?("port")
-                key(key.path.keys + [:ingress, ingress_index, :source]).failure("must be an integer") unless port[:source].is_a?(::Integer)
-              elsif port[:type].eql?("port_range")
-                key(key.path.keys + [:ingress, ingress_index, :source]).failure("must be an array of integers") unless port[:source].is_a?(::Array)
-                if port[:source].is_a?(::Array)
-                  key(key.path.keys + [:ingress, ingress_index, :source]).failure("size of source array must be greater than or equal to the container count") if value[:container_count].present? && port[:source].size < value[:container_count]
-                  port[:source].each_with_index do |source, index|
-                    key(key.path.keys + [:ingress, ingress_index, :source]).failure("element #{index} in the array must be an integer") unless source.is_a?(::Integer)
+            value[:ingress].each_with_index do |ingress, ingress_index|
+              source_key = key([:roles, index, :ingress, ingress_index, :source])
+              if ingress[:type].eql?("port")
+                source_key.failure("must be an integer") unless ingress[:source].is_a?(::Integer)
+              elsif ingress[:type].eql?("port_range")
+                source_key.failure("must be an array of integers") unless ingress[:source].is_a?(::Array)
+                if ingress[:source].is_a?(::Array)
+                  source_key.failure("size of source array must be greater than or equal to the container count") if value[:container_count].present? && ingress[:source].size < value[:container_count]
+                  ingress[:source].each_with_index do |source, source_index|
+                    source_key.failure("the element at index #{source_index} in the array must be an integer") unless source.is_a?(::Integer)
                   end
                 end
               end
--- spec/cove/configuration/contracts/service_contract_spec.rb
+++ spec/cove/configuration/contracts/service_contract_spec.rb
@@ -9,12 +9,14 @@ RSpec.describe Cove::Configuration::Contracts::ServiceContract do
     it "fails when service name is not filled" do
       contract = Cove::Configuration::Contracts::ServiceContract.new
       result = contract.call(service_name: "", image: "nginx:1.23.4", roles: [{name: "rolename", container_count: 3}])
+      expect(result.errors[:service_name]).to be_present
       expect(result.success?).to eq(false)
     end
 
     it "fails when image is not filled" do
       contract = Cove::Configuration::Contracts::ServiceContract.new
       result = contract.call(service_name: "nginx", image: "", roles: [{name: "rolename", container_count: 3}])
+      expect(result.errors[:image]).to be_present
       expect(result.success?).to eq(false)
     end
   end
@@ -23,12 +25,7 @@ RSpec.describe Cove::Configuration::Contracts::ServiceContract do
     it "fails when roles is empty" do
       contract = Cove::Configuration::Contracts::ServiceContract.new
       result = contract.call(service_name: "a", image: "b", roles: [])
-      expect(result.success?).to eq(false)
-    end
-
-    it "fails when a role does not have a name" do
-      contract = Cove::Configuration::Contracts::ServiceContract.new
-      result = contract.call(service_name: "a", image: "b", roles: [{container_count: 2}])
+      expect(result.errors[:roles]).to be_present
       expect(result.success?).to eq(false)
     end
 
@@ -36,6 +33,22 @@ RSpec.describe Cove::Configuration::Contracts::ServiceContract do
       contract = Cove::Configuration::Contracts::ServiceContract.new
       result = contract.call(service_name: "nginx", image: "nginx:1.23.4", roles: [{name: "web1", container_count: "2"}, {name: "web2", container_count: "2"}])
       expect(result.success?).to eq(true)
+    end
+
+    context "name" do
+      it "fails when a role does not have a name" do
+        contract = Cove::Configuration::Contracts::ServiceContract.new
+        result = contract.call(service_name: "a", image: "b", roles: [{container_count: 2}])
+        expect(result.errors[:roles][0][:name]).to be_present
+        expect(result.success?).to eq(false)
+      end
+
+      it "fails with duplicate role names" do
+        contract = Cove::Configuration::Contracts::ServiceContract.new
+        result = contract.call(service_name: "nginx", image: "nginx:1.23.4", roles: [{name: "web"}, {name: "web"}, {name: "web2"}])
+        expect(result.errors[:roles][1][:name]).to be_present
+        expect(result.success?).to eq(false)
+      end
     end
 
     context "container_count" do
@@ -48,6 +61,7 @@ RSpec.describe Cove::Configuration::Contracts::ServiceContract do
       it "fails when container_count is not an integer" do
         contract = Cove::Configuration::Contracts::ServiceContract.new
         result = contract.call(service_name: "a", image: "b", roles: [{name: "web", container_count: "abc"}])
+        expect(result.errors[:roles][0][:container_count]).to be_present
         expect(result.success?).to eq(false)
       end
     end
@@ -68,54 +82,63 @@ RSpec.describe Cove::Configuration::Contracts::ServiceContract do
       it "fails when type is not included" do
         contract = Cove::Configuration::Contracts::ServiceContract.new
         result = contract.call(service_name: "a", image: "b", roles: [{name: "web", ingress: [{source: [8080, 8081], target: 80}]}])
+        expect(result.errors[:roles][0][:ingress][0][:type]).to be_present
         expect(result.success?).to eq(false)
       end
 
       it "fails when type is not port or port_range" do
         contract = Cove::Configuration::Contracts::ServiceContract.new
         result = contract.call(service_name: "a", image: "b", roles: [{name: "web", ingress: [{type: "something", source: [8080, 8081], target: 80}]}])
+        expect(result.errors[:roles][0][:ingress][0][:type]).to be_present
         expect(result.success?).to eq(false)
       end
 
       it "fails when source is not included" do
         contract = Cove::Configuration::Contracts::ServiceContract.new
         result = contract.call(service_name: "a", image: "b", roles: [{name: "web", ingress: [{type: "port_range", target: 80}]}])
+        expect(result.errors[:roles][0][:ingress][0][:source]).to be_present
         expect(result.success?).to eq(false)
       end
 
       it "fails when source is not an integer" do
         contract = Cove::Configuration::Contracts::ServiceContract.new
         result = contract.call(service_name: "a", image: "b", roles: [{name: "web", ingress: [{type: "port", source: "eighty", target: 80}]}])
+        expect(result.errors[:roles][0][:ingress][0][:source]).to be_present
         expect(result.success?).to eq(false)
       end
 
       it "fails when source is not an array" do
         contract = Cove::Configuration::Contracts::ServiceContract.new
         result = contract.call(service_name: "a", image: "b", roles: [{name: "web", ingress: [{type: "port_range", source: 8080, target: 80}]}])
+        expect(result.errors[:roles][0][:ingress][0][:source]).to be_present
         expect(result.success?).to eq(false)
       end
 
       it "fails when source is an array of non-integers" do
         contract = Cove::Configuration::Contracts::ServiceContract.new
         result = contract.call(service_name: "a", image: "b", roles: [{name: "web", ingress: [{type: "port_range", source: [8080, "abc"], target: 80}]}])
+        expect(result.errors[:roles][0][:ingress][0][:source]).to be_present
         expect(result.success?).to eq(false)
       end
 
       it "fails when the size of the source array is less than the container count " do
         contract = Cove::Configuration::Contracts::ServiceContract.new
         result = contract.call(service_name: "a", image: "b", roles: [{name: "web", container_count: 4, ingress: [{type: "port_range", source: [8080, 8081], target: 80}]}])
+        expect(result.errors[:roles][0][:ingress][0][:source]).to be_present
         expect(result.success?).to eq(false)
       end
 
       it "fails when target is not included" do
         contract = Cove::Configuration::Contracts::ServiceContract.new
         result = contract.call(service_name: "a", image: "b", roles: [{name: "web", ingress: [{type: "port", source: 8080}]}])
+        expect(result.errors[:roles][0][:ingress][0][:target]).to be_present
         expect(result.success?).to eq(false)
       end
 
       it "fails when target is not an integer" do
         contract = Cove::Configuration::Contracts::ServiceContract.new
         result = contract.call(service_name: "a", image: "b", roles: [{name: "web", ingress: [{type: "port", source: 8080, target: "abc"}]}])
+        expect(result.errors[:roles][0][:ingress][0][:target]).to be_present
         expect(result.success?).to eq(false)
       end
     end

dee32256c69ab5e1cc8f8d345fd952a00ee421da
Julian Fahrer <julian@fahrer.net>
2023-11-27 21:45:53 -0800

    resolver specs

--- lib/cove/deployment_config/file_resolver.rb
+++ lib/cove/deployment_config/file_resolver.rb
@@ -1,16 +1,28 @@
 module Cove
   class DeploymentConfig
     class FileResolver
+      class FileNotFound < Cove::Error
+        def initialize(path)
+          super("File not found: #{path}")
+        end
+      end
+
       def call(registry:, deployment:, source:)
         path = File.join(deployment.directory, source)
         renderer = Renderer.new(registry, deployment)
 
+        raise FileNotFound.new(path) unless File.exist?(path)
+
         if File.directory?(path)
-          Dir.glob(File.join(path, "**/*"))
+          Dir.glob(File.join(path, "**/*")).select do |file|
+            File.file?(file)
+          end.map do |file|
+            [file.gsub(/^#{deployment.directory}/, ""), renderer.call(File.read(file))]
+          end
         else
-          [path]
-        end.map do |file|
-          [file.gsub(/^#{deployment.directory}/, ""), renderer.call(file)]
+          [
+            [File.basename(path), renderer.call(File.read(path))]
+          ]
         end.map do |path, content|
           ConfigFile.new(path: path, content: content)
         end

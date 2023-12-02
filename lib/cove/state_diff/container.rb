cd60734c4b35e226c708505078115f01e1e5b269
Julian Fahrer <julian@fahrer.net>
2023-12-02 08:54:46 -0800

    wip

--- lib/cove/state_diff/container.rb
+++ lib/cove/state_diff/container.rb
@@ -4,22 +4,23 @@
 # It will perform a rolling restart where it first stops an existing container
 # (if applicable) and then starts a new one before stopping the next one.
 #
-# If the number of desired containers is less than the number of running it first
-# reduces the number of running containers to match the number of desired containers
-# before continueing with a rolling restart.
+# If the number of desired containers is less than the number of running
+# containers it first reduces the number of running containers to match the
+# number of desired containers before continuing with a rolling restart for the
+# remaining containers.
 #
-# If the number of desired containers is greater than the number of running it first
-# start a new container before continuing with a rolling restart.
+# If the number of desired containers is greater than the number of running
+# containers it first start new containers to match the number of desired
+# containers before continuing with a rolling restart.
 #
-# The containers version and index is used to determine whether it already exists. The running container
-# instance also responds to `running?` to determine whether it is running or not.
+# The containers version and index is used to determine whether it already exists.
 #
 # The set of instructions returned follows the following format:
 # [
-#  { :action => :stop, :container => <container> },
-#  { :action => :start, :container => <container> },
-#  { :action => :stop, :container => <container> },
-#  { :action => :start, :container => <container> },
+#  { action: :stop, container: => <container name> },
+#  { action: :start, container: => <container name> },
+#  { action: :stop, container: => <container name> },
+#  { action: :start, container: => <container name> },
 # ]
 
 module Cove
@@ -40,33 +41,29 @@ module Cove
         # If we wanted to implement other strategies we could do that here.
         # E.g. start first, stop all
         if running_containers.size >= desired_containers.size
-          padded_start_instructions = post_pad(start_instructions, stop_instructions.size)
+          padded_start_instructions = padding(stop_instructions.size - start_instructions.size) + start_instructions
           stop_instructions.zip(padded_start_instructions).flatten.compact
         else
-          padded_stop_instructions = pre_pad(stop_instructions, start_instructions.size)
+          padded_stop_instructions = stop_instructions + padding(start_instructions.size - stop_instructions.size)
           start_instructions.zip(padded_stop_instructions).flatten.compact
         end
       end
 
       private
 
-      def pre_pad(array, size)
-        array + [nil] * (size - array.size)
-      end
-
-      def post_pad(array, size)
-        [nil] * (size - array.size) + array
+      def padding(size)
+        [nil] * size
       end
 
       def verify!
-        found_containers = desired_containers.map do |desired_container|
-          current_containers.find do |current_container|
+        missing_containers = desired_containers.reject do |desired_container|
+          current_containers.any? do |current_container|
             current_container.index == desired_container.index &&
               current_container.version == desired_container.version
           end
-        end.compact
+        end
 
-        raise Error.new("Missing Container") if found_containers.size < desired_containers.size
+        raise Error.new("Can't construct steps to reconcile container statuses, the following containers are missing: #{missing_containers.map(&:name)}") if missing_containers.any?
       end
 
       def stop_instructions
